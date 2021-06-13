b69adeb1ff81618216b528719b61198f78e69755
september21st <september21st@users.noreply.github.com>
2021-06-14 01:27:56 +0900

    feat: Add kakao Adfit

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { infoList } from '../data.js';
@@ -7,6 +7,24 @@ import './Result.css';
 function Result() {
   const { type } = useParams();
   const data = infoList[type - 1];
+
+  useEffect(() => {
+    let ins = document.createElement('ins');
+    let scr = document.createElement('script');
+
+    ins.className = 'kakao_ad_area';
+    ins.style = 'display:none; width:100%;';
+    scr.async = 'true';
+    scr.type = 'text/javascript';
+    scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+    ins.setAttribute('data-ad-width', '320');
+    ins.setAttribute('data-ad-height', '100');
+    ins.setAttribute('data-ad-unit', 'DAN-NAQztt31OlW16JM5');
+
+    document.querySelector('.adfit').appendChild(ins);
+    document.querySelector('.adfit').appendChild(scr);
+  }, []);
+
   return (
     <>
       <div className="result-box">
@@ -27,6 +45,7 @@ function Result() {
           </button>
         </Link>
       </div>
+      <div className="adfit"></div>
     </>
   );
 }
