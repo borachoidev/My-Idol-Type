62845d64e5e4f15e76147aed8b0c2a1109961166
september21st <september21st@users.noreply.github.com>
2021-06-14 01:54:14 +0900

    chore: Remove kakao adfit

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -8,22 +8,22 @@ function Result() {
   const { type } = useParams();
   const data = infoList[type - 1];
 
-  useEffect(() => {
-    let ins = document.createElement('ins');
-    let scr = document.createElement('script');
+  // useEffect(() => {
+  //   let ins = document.createElement('ins');
+  //   let scr = document.createElement('script');
 
-    ins.className = 'kakao_ad_area';
-    ins.style = 'display:none; width:100%;';
-    scr.async = 'true';
-    scr.type = 'text/javascript';
-    scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
-    ins.setAttribute('data-ad-width', '320');
-    ins.setAttribute('data-ad-height', '50');
-    ins.setAttribute('data-ad-unit', 'DAN-iNS6cTHEaa2gLNKW');
+  //   ins.className = 'kakao_ad_area';
+  //   ins.style = 'display:none; width:100%;';
+  //   scr.async = 'true';
+  //   scr.type = 'text/javascript';
+  //   scr.src = '//t1.daumcdn.net/kas/static/ba.min.js';
+  //   ins.setAttribute('data-ad-width', '320');
+  //   ins.setAttribute('data-ad-height', '50');
+  //   ins.setAttribute('data-ad-unit', '광고단위코드');
 
-    document.querySelector('.adfit').appendChild(ins);
-    document.querySelector('.adfit').appendChild(scr);
-  }, []);
+  //   document.querySelector('.adfit').appendChild(ins);
+  //   document.querySelector('.adfit').appendChild(scr);
+  // }, []);
 
   return (
     <>
