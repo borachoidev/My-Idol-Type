4372a3b2f60f519a7c5aac66273bfbbee90b0e85
september21st <september21st@users.noreply.github.com>
2021-06-16 23:21:47 +0900

    fix: Fix meta tag for SEO

--- src/routes/Result.js
+++ src/routes/Result.js
@@ -38,22 +38,30 @@ function Result() {
         <meta
           name="description"
           content={data.name + '| 나의 아이돌 유형도 알아보기!'}
+          data-react-helmet="true"
         />
         <meta
           name="keywords"
           content="아이돌유형,아이돌mbti,아이돌테스트,mitt"
+          data-react-helmet="true"
         />
         <meta
           property="og:title"
           content="결과 보기 | MITT 내아이돌유형테스트"
+          data-react-helmet="true"
         />
-        <meta property="og:type" content="article" />
-        <meta property="og:site_name" content="MITT 내아이돌유형테스트" />
+        <meta property="og:type" content="article" data-react-helmet="true" />
+        <meta
+          property="og:site_name"
+          content="MITT 내아이돌유형테스트"
+          data-react-helmet="true"
+        />
         <meta
           property="og:description"
           content={data.name + '| 나의 아이돌 유형도 알아보기!'}
+          data-react-helmet="true"
         />
-        <meta property="og:image" content={data.og} />
+        <meta property="og:image" content={data.og} data-react-helmet="true" />
         <script
           type="text/javascript"
           src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-60ca01f6b5f03663"
