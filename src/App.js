cde8f956b87b78356ce60dadfef86d58e76d4ad2
september21st <september21st@users.noreply.github.com>
2021-06-18 00:07:44 +0900

    feat: Add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './routes/Home';
 import Result from './routes/Result';
 import './App.css';
 import Test from './routes/Test';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route exact={true} path="/" component={Home} />
       <Route path="/test" component={Test} />
       <Route path="/result/:type" component={Result} />
+      <Footer />
     </HashRouter>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(props) {
+  return (
+    <footer>
+      <span>Copyright 2021. Bora Choi all rights reserved.</span>
+    </footer>
+  );
+}
+
+export default Footer;
