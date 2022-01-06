cef35182012a6c7e6433d1625c46d3500ee7f986
september21st <tsuilook@gmail.com>
2022-01-06 17:57:57 +0900

    :lipstick: 광고 위치 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,17 @@ import Layout from '../components/Layout'
 
 function Home() {
   return (
-    <main>
+    <main tw="space-y-5">
       <Image width={500} height={500} src={`/images/main.gif`} alt="미리보기" />
 
-      <p tw="text-sm word-break[keep-all] p-8">
-        한국을 넘어서 전 세계사람들의 마음을 사로잡은 K-pop 아이돌!
+      <p tw="text-sm word-break[keep-all] text-lg px-2 text-center">
+        한국을 넘어서 전 세계사람들의 <br />
+        마음을 사로잡은
+        <span tw="text-xl text-gray-500"> K-pop 아이돌!</span>
         <br />
         내가 만약 데뷔를 한다면 나는 어떤 아이돌일까 ? <br />
-        지금 나의 아이돌 유형을 확인하려면 <b>"시작하기"</b> 버튼을 눌러주세요!
+        지금 나의 아이돌 유형을 확인하려면 <br />
+        <span tw="text-gray-500 ">"시작하기"</span> 버튼을 눌러주세요!
       </p>
 
       <Link href="/test">
