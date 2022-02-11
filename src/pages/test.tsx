7eac5ef0b6e5c7e5d65a741a611cf37453b7f44d
september21st <tsuilook@gmail.com>
2022-02-11 17:39:30 +0900

    :mag: 모든 페이지 메타태그 추가

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -3,6 +3,7 @@ import Answer from '../components/Answer'
 import Layout from '../components/Layout'
 import { qnaList } from '../data'
 import 'twin.macro'
+import Metatag from '~/components/Metatag'
 
 const Test = () => {
   const [step, setStep] = useState<number>(0)
@@ -28,33 +29,25 @@ const Test = () => {
   })
 
   return (
-    <section tw="px-4">
-      <span>{step + 1}/12</span>
-      <div tw="bg-gray-100 rounded-full overflow-hidden height[10px]">
-        <div
-          style={{ width: `${step * 8.333}%` }}
-          tw="bg-pink-700 transition-all"
-        >
-          &nbsp;
+    <>
+      <Metatag />
+      <section tw="px-4">
+        <span>{step + 1}/12</span>
+        <div tw="bg-gray-100 rounded-full overflow-hidden height[10px]">
+          <div style={{ width: `${step * 8.333}%` }} tw="bg-pink-700 transition-all">
+            &nbsp;
+          </div>
         </div>
-      </div>
-      <article tw="p-5 text-center word-break[keep-all] text-xl text-pink-500 height[120px]">
-        <p>{qnaList[step].q}</p>
-      </article>
+        <article tw="p-5 text-center word-break[keep-all] text-xl text-pink-500 height[120px]">
+          <p>{qnaList[step].q}</p>
+        </article>
 
-      <article tw="text-center space-y-4">
-        <Answer
-          answer={qnaList[step].a[0]}
-          steps={[step, setStep]}
-          types={[type, setType]}
-        />
-        <Answer
-          answer={qnaList[step].a[1]}
-          steps={[step, setStep]}
-          types={[type, setType]}
-        />
-      </article>
-    </section>
+        <article tw="text-center space-y-4">
+          <Answer answer={qnaList[step].a[0]} steps={[step, setStep]} types={[type, setType]} />
+          <Answer answer={qnaList[step].a[1]} steps={[step, setStep]} types={[type, setType]} />
+        </article>
+      </section>
+    </>
   )
 }
 
