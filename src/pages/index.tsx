e920a0801a8c6d355cd27ddf1aeb349bd481fc1f
september21st <tsuilook@gmail.com>
2022-02-26 00:29:01 +0900

    :globe_with_meridians: 시작화면 국제화

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,27 @@
 import React, { ReactElement } from 'react'
+import { GetStaticProps } from 'next'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 import 'twin.macro'
 
 import Layout from '../components/Layout'
 import Metatag from '~/components/Metatag'
 
 function Home() {
+  const { t } = useTranslation()
   return (
     <>
       <Metatag />
       <main tw="space-y-5">
         <Image width={425} height={425} src={`/images/main.gif`} alt="미리보기" />
 
-        <p tw="text-sm word-break[keep-all] text-lg px-2 text-center">
-          한국을 넘어서 전 세계사람들의 <br />
-          마음을 사로잡은
-          <span tw="text-xl text-pink-500"> K-pop 아이돌!</span>
-          <br />
-          내가 만약 데뷔를 한다면 나는 어떤 아이돌일까 ? <br />
-          지금 나의 아이돌 유형을 확인하려면 <br />
-          <span tw="text-pink-500 ">"시작하기"</span> 버튼을 눌러주세요!
-        </p>
+        <p tw="text-sm word-break[keep-all] text-lg px-2 text-center">{t('intro:intro')}</p>
         <div tw="px-3">
           <Link href="/test">
             <button type="button" tw="py-4 w-full bg-gray-500 rounded text-pink-300">
-              시작하기
+              {t('intro:start-button')}
             </button>
           </Link>
         </div>
@@ -35,6 +31,10 @@ function Home() {
 }
 
 export default Home
+
+export const getStaticProps: GetStaticProps = async ({ locale = 'ko' }) => {
+  return { props: { ...(await serverSideTranslations(locale, ['intro'])) } }
+}
 Home.getLayout = function getLayout(page: ReactElement) {
   return <Layout>{page}</Layout>
 }
