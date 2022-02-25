f53ca574f35ba2f99d1afd8babfc0d362cc8ae34
september21st <tsuilook@gmail.com>
2022-02-26 01:13:48 +0900

    :globe_with_meridians: 결과 페이지 국제화

--- src/pages/result/[type].tsx
+++ src/pages/result/[type].tsx
@@ -9,12 +9,15 @@ import { ITestResult } from '~/types/data'
 import Layout from '~/components/Layout'
 import Metatag from '~/components/Metatag'
 import Share from '~/components/Share'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useTranslation } from 'next-i18next'
 
 interface ResultProps {
   result: ITestResult
   type: string
 }
 function Result({ result, type }: ResultProps) {
+  const { t } = useTranslation()
   const pageUrl = useMemo(() => `https://my-idol-type-test.vercel.app/result/${type}`, [])
 
   useEffect(() => {
@@ -42,33 +45,35 @@ function Result({ result, type }: ResultProps) {
   if (!result) return null
   return (
     <>
-      <Metatag title={result.name} description={result.desc} image={result.og} />
+      <Metatag title={t(`result:${result.name}`)} description={t(`result:${result.desc}`)} image={result.og} />
       <section tw="space-y-3">
         <div tw="p-3 space-y-3">
-          <h1 tw="text-2xl font-bold word-break[keep-all] px-5 text-pink-500 text-center">{result.name}</h1>
+          <h1 tw="text-2xl font-bold word-break[keep-all] px-5 text-pink-500 text-center">
+            {t(`result:${result.name}`)}
+          </h1>
           <div>
             <Image width={425} height={425} src={`/images/${result.id}.jpg`} alt={result.name} />
           </div>
 
           <div tw="px-3 text-center">
-            <p tw="whitespace-pre-line word-break[keep-all] text-center">{result.desc}</p>
+            <p tw="whitespace-pre-line word-break[keep-all] text-center">{t(`result:${result.desc}`)}</p>
           </div>
         </div>
 
         <div tw="p-3 text-center space-y-1">
-          <h3 tw="font-bold text-lg text-pink-500">나와 캐미터지는 아이돌은 ?</h3>
-          <p>{result.partner}</p>
+          <h3 tw="font-bold text-lg text-pink-500">{t('result:chemistry')}</h3>
+          <p>{t(`result:${result.partner}`)}</p>
         </div>
 
         <Link href="/types">
           <div role={'button'} tw="underline text-center text-gray-500">
-            다른 유형 확인하기
+            {t('result:check-other-type')}
           </div>
         </Link>
         <div tw="p-3">
           <Link href="/test">
             <button type="button" tw="py-4 w-full bg-gray-500 rounded text-pink-300">
-              다시하기
+              {t('result:retry-button')}
             </button>
           </Link>
         </div>
@@ -114,9 +119,8 @@ const DefaultLinkButton = styled.div`
   cursor: pointer;
 `
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps = async ({ locale = 'ko', params }) => {
   const { data: testResult } = await import('~/data/result.json')
-  const { params } = context
 
   if (!params)
     return {
@@ -124,21 +128,33 @@ export const getStaticProps: GetStaticProps = async (context) => {
     }
 
   return {
-    props: { result: testResult[Number(params.type) - 1], type: params.type },
+    props: {
+      result: testResult[Number(params.type) - 1],
+      type: params.type,
+      ...(await serverSideTranslations(locale, ['result'])),
+    },
   }
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [
-      { params: { type: '1' } },
-      { params: { type: '2' } },
-      { params: { type: '3' } }, // See the "paths" section below
-      { params: { type: '4' } },
-      { params: { type: '5' } },
-      { params: { type: '6' } },
-      { params: { type: '7' } },
-      { params: { type: '8' } },
+      { params: { type: '1' }, locale: 'ko' },
+      { params: { type: '2' }, locale: 'ko' },
+      { params: { type: '3' }, locale: 'ko' },
+      { params: { type: '4' }, locale: 'ko' },
+      { params: { type: '5' }, locale: 'ko' },
+      { params: { type: '6' }, locale: 'ko' },
+      { params: { type: '7' }, locale: 'ko' },
+      { params: { type: '8' }, locale: 'ko' },
+      { params: { type: '1' }, locale: 'ja' },
+      { params: { type: '2' }, locale: 'ja' },
+      { params: { type: '3' }, locale: 'ja' },
+      { params: { type: '4' }, locale: 'ja' },
+      { params: { type: '5' }, locale: 'ja' },
+      { params: { type: '6' }, locale: 'ja' },
+      { params: { type: '7' }, locale: 'ja' },
+      { params: { type: '8' }, locale: 'ja' },
     ],
     fallback: false, // See the "fallback" section below
   }
