380a82a1df9096869a291fad5346228797d8405c
september21st <tsuilook@gmail.com>
2022-02-25 18:08:06 +0900

    :bento: cdn 주소 변경

--- src/constants.ts
+++ src/constants.ts
@@ -3,5 +3,5 @@ export const FB_APP_ID = process.env.NEXT_PUBLIC_FB_APP_ID
 export const GA_TRACKING_ID = process.env.NEXT_PIBLIC_GA_TRACKING_ID
 const PRODUCTION = process.env.NODE_ENV === 'production'
 export const MAIN_IMG_CDN = PRODUCTION
-  ? 'https://rawcdn.githack.com/borachoidev/My-Idol-Type/9db39f52240ecc175551323967d908cdc78fd6f0/public/images/main.gif?raw=true'
-  : 'https://raw.githack.com/borachoidev/My-Idol-Type/master/public/images/main.gif?raw=true'
+  ? 'https://rawcdn.githack.com/borachoidev/My-Idol-Type/refactor/image/public/images/main.gif?raw=true'
+  : 'https://raw.githack.com/borachoidev/My-Idol-Type/refactor/image/public/images/main.gif?raw=true'
